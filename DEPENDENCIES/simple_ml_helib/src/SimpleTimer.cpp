72db823967c35b295a454683dfdf1e41407ea60a
Dmitry Tsarevich <dimhotepus@gmail.com>
2020-08-14 16:08:56 +0300

    Do not miss timer measure interval when it > INT_MAX
    std::chrono::duration<>.count() can be int64_t, so we can compute
    measured microseconds incorrectly in case microsecs > INT_MAX.
    
    Note sum and sumSquares already int64_t.
    
    Signed-off-by: Dmitry Tsarevich <dimhotepus@gmail.com>

--- DEPENDENCIES/simple_ml_helib/src/SimpleTimer.cpp
+++ DEPENDENCIES/simple_ml_helib/src/SimpleTimer.cpp
@@ -134,14 +134,14 @@ void SimpleTimer::stop() {
 void SimpleTimer::SectionInfo::addMeasure(
     std::chrono::high_resolution_clock::time_point s, int64_t cpu_s) {
   auto end_time = high_resolution_clock::now();
-  int mircosecs = duration_cast<microseconds>(end_time - s).count();
+  int64_t microsecs = duration_cast<microseconds>(end_time - s).count();
 
   auto end_cpu = getProcessCPUTime();
   int64_t cpu_microsecs = (end_cpu - cpu_s) / 1000;
 
   const lock_guard<mutex> lock(mtx);
-  sum += mircosecs;
-  sumSquares += mircosecs * mircosecs;
+  sum += microsecs;
+  sumSquares += microsecs * microsecs;
   count += 1;
   sumCPU += cpu_microsecs;
 }
