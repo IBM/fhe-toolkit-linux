9065cc8d55c2406da2ffc274224587cab6e5415b
Rosie Lickorish <rosielickorish@gmail.com>
2020-08-20 22:00:50 +0100

    Update test script

--- samples/BGV_country_db_lookup/runtest.sh
+++ samples/BGV_country_db_lookup/runtest.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
-# Copyright (C) 2020 IBM Corp.
-# This program is Licensed under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance
-# with the License. You may obtain a copy of the License at
-#   http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License. See accompanying LICENSE file.
+# MIT License
+# 
+# Copyright (c) 2020 International Business Machines
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-queries=('Albania:Tirana'
-         'Austria:Vienna'
-         'Belarus:Minsk'
-         'Southampton:Country'
-        )
+country_capitals=('Albania:Tirana' 'Austria:Vienna' 'Belarus:Minsk' 'Southampton:Country')
 
 # Number of queries
-rt=${#queries[@]}
+rc=${#country_capitals[@]}
 
-for query in "${queries[@]}"; do
+for country_capital in "${country_capitals[@]}"; do
   # Capture the result value for comparison
-  value=$( $PWD/build/BGV_country_db_lookup <<< "${query%:*}" | awk '/Query result:/{ print $3 }' )
+  query="${country_capital%:*}"
+  capital=$( $PWD/build/BGV_country_db_lookup.main <<< $query | awk '/Query result:/{ print $3 }' )
 
-  echo "${query%:*} gives '$value'"
-  # If the query result matches then decrement the return value rt
-  if [ "$value" = "${query#*:}" ]; then
-    rt=$((rt-1))
+  echo "$query returns '$capital'"
+  if [ "$capital" = "${country_capital#*:}" ]; then 
+    let rc--;
+  else
+    echo "Test failed.."
+    echo "Expected: $country_capital"
+    echo "Actual: $query:$capital"
+    echo ""
   fi
 done
 
 # This will return the overall pass or fail
-exit "$rt"
+echo $rc
+exit $rc
