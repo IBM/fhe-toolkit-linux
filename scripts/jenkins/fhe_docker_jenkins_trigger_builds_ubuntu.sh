3903f71c8d69ce73e2be461e2a5aa30399648a8c
Gregory Boland <boland@us.ibm.com>
2020-09-03 16:34:40 -0400

    changing script to pull from this repo

--- scripts/jenkins/fhe_docker_jenkins_trigger_builds_ubuntu.sh
+++ scripts/jenkins/fhe_docker_jenkins_trigger_builds_ubuntu.sh
@@ -34,7 +34,7 @@ ARTE_USER=$1
 ARTE_PWD=$2
 
 # Pull latest from the FHE repo, master branch
-git checkout master
+git checkout feature/jenkins_tests_and_tags
 # Build the Docker image for Ubuntu
 ./BuildDockerImage.sh ubuntu
 # Shut everything down before we start
