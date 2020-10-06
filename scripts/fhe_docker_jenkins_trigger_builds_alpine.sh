66b0e4739279b1456de2c17a308ac3bc45eaa65e
Gregory Boland <boland@us.ibm.com>
2020-10-06 16:56:02 -0400

    added in alpine building

--- scripts/fhe_docker_jenkins_trigger_builds_alpine.sh
+++ scripts/fhe_docker_jenkins_trigger_builds_alpine.sh
@@ -27,11 +27,44 @@ pushd ../../
 set -x 
 set -u
 set -e
+
+
+source ConfigConstants.sh
+ARTE_USER=$1
+ARTE_PWD=$2
+
 # Pull latest from the FHE repo, master branch
-git checkout master
+git checkout nigthly_builds
 # Build the Docker image for Alpine
 ./BuildDockerImage.sh alpine
+# Shut everything down before we start
+./StopToolkit.sh
 # Run the toolkit container based on the Alpine image
 ./RunToolkit.sh -l -s alpine
+# Test sample runs as expected
+docker exec local-fhe-toolkit-alpine /bin/bash -c " \
+    cd /opt/IBM/FHE-Workspace; \
+    mkdir build; cd build; \
+    cmake ../examples/BGV_country_db_lookup;\
+    make;\
+    cd ..;\
+    chmod 755 examples/BGV_country_db_lookup/runtest.sh;\
+    ./examples/BGV_country_db_lookup/runtest.sh;"
 # Shut everything down 
 ./StopToolkit.sh
+
+
+NOW=$(date +'%m-%d-%Y')
+NIGHTLY_SUFFIX="nightly-${NOW}"
+VERSION="$HElib_version.$TOOLKIT_VERSION"
+
+#Login to Artifactory using the fhe user
+echo "DOCKER LOGIN"
+#docker login -u $ARTE_USER -p $ARTE_PWD "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com"
+echo $ARTE_PWD | docker login -u $ARTE_USER --password-stdin "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com"
+#Tag the docker build for storage in Artifactory
+docker tag "local/fhe-toolkit-alpine-amd64:latest" "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/alpine/fhe-toolkit-alpine-amd64:$VERSION-$NIGHTLY_SUFFIX"
+echo "tagging it"
+#Push and save the newly tagged build in Artifactory
+docker push "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/alpine/fhe-toolkit-alpine-amd64:$VERSION-$NIGHTLY_SUFFIX"
+echo "pushing it"
