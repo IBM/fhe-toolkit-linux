4ff62536e6090483ee1ac8f24a43b90918f1caac
Gregory Boland <boland@us.ibm.com>
2020-10-05 22:53:43 -0400

    trying a new way to build the version number

--- scripts/jenkins/fhe_docker_jenkins_trigger_builds_centos.sh
+++ scripts/jenkins/fhe_docker_jenkins_trigger_builds_centos.sh
@@ -55,13 +55,14 @@ docker exec local-fhe-toolkit-centos /bin/bash -c " \
 
 NOW=$(date +'%m-%d-%Y')
 NIGHTLY_SUFFIX="nightly-${NOW}"
+VERSION="$HElib_version.$TOOLKIT_VERSION"
 #Login to Artifactory using the fhe user
 echo "DOCKER LOGIN"
 #docker login -u $ARTE_USER -p $ARTE_PWD "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com"
 echo $ARTE_PWD | docker login -u $ARTE_USER --password-stdin "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com"
 #Tag the docker build for storage in Artifactory
-docker tag "local/fhe-toolkit-centos-amd64:latest" "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/centos/fhe-toolkit-centos-amd64:$FULL_TOOLKIT_VERSION-$NIGHTLY_SUFFIX"
+docker tag "local/fhe-toolkit-centos-amd64:latest" "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/centos/fhe-toolkit-centos-amd64:$VERSION-$NIGHTLY_SUFFIX"
 echo "tagging it"
 #Push and save the newly tagged build in Artifactory
-docker push "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/centos/fhe-toolkit-centos-amd64:$FULL_TOOLKIT_VERSION-$NIGHTLY_SUFFIX"
+docker push "sys-ibm-fhe-team-linux-docker-local.artifactory.swg-devops.com/centos/fhe-toolkit-centos-amd64:$VERSION-$NIGHTLY_SUFFIX"
 echo "pushing it"
