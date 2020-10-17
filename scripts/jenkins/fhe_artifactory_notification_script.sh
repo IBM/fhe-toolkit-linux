5991a84df433d13d46cfdeef44391c022f62f802
Gregory Boland <boland@us.ibm.com>
2020-10-16 21:51:57 -0400

    set up commit url in notification to slack

--- scripts/jenkins/fhe_artifactory_notification_script.sh
+++ scripts/jenkins/fhe_artifactory_notification_script.sh
@@ -4,12 +4,13 @@ SLACK_HOOK=$1
 BUILD_KIND=$2
 BUILD_TYPE=$3
 ARTE_URL=$4
+COMMIT_URL=""
 
 generate_post_message()
 {
   cat <<EOF
 {
-  "text":"A new build of $BUILD_KIND for $BUILD_TYPE based on GitHub commit: $COMMIT_NUMBER, passed all the tests and is now available for docker pull at <$ARTE_URL>"
+  "text":"A new build of $BUILD_KIND for $BUILD_TYPE based on GitHub commit: <$COMMIT_URL|$COMMIT_NUMBER>, passed all the tests and is now available for docker pull at <$ARTE_URL>"
 }
 EOF
 }
@@ -17,7 +18,14 @@ EOF
 get_commit_url()
 {
   #Make HTTPS
-  BASE_URL="git config --get remote.origin.url | sed -e 's/:/\//g'| sed -e 's/git@/https:\/\//g'"
+  suffix=".git"
+  GIT_BASE=$(git config --get remote.origin.url)
+  GIT_BASE=${GIT_BASE%"$suffix"}
+  BASE_URL=$(git config --get remote.origin.url | sed -e 's/:/\//g'| sed -e 's/git@/https:\/\//g')
+  GIT_BASE=${BASE_URL%"$suffix"}
+  origin_head=$(git ls-remote --heads $(git config --get remote.origin.url) | grep "refs/heads/master" | cut -f 1)
+  COMMIT_URL=$GIT_BASE"/commit/$origin_head"
+  echo $COMMIT_URL
 }
 
 
@@ -25,7 +33,7 @@ get_commit_url()
 COMMIT_NUMBER="$(git log -1 --pretty=format:"%h")"
 
 
-
+get_commit_url
 echo $(generate_post_message)
 curl -X POST -H 'Content-type: application/json' --data "$(generate_post_message)" $SLACK_HOOK
 
