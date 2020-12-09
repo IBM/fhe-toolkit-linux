590721c9fed5062cfb9aedc195e00bbd22d0942e
Gregory Boland <boland@us.ibm.com>
2020-12-09 15:23:22 -0500

    shutting down running containers

--- scripts/jenkins/fhe_docker_jenkins_trigger_builds_ubuntu.sh
+++ scripts/jenkins/fhe_docker_jenkins_trigger_builds_ubuntu.sh
@@ -53,13 +53,14 @@ docker exec local-fhe-toolkit-ubuntu /bin/bash -c " \
     cd ..;\
     chmod 755 examples/BGV_country_db_lookup/runtest.sh;\
     ./examples/BGV_country_db_lookup/runtest.sh;"
-# Shut everything down 
-./StopToolkit.sh
 
 NOW=$(date +'%m-%d-%Y')
 NIGHTLY_SUFFIX="nightly-${NOW}"
 VERSION="$HElib_version.$TOOLKIT_VERSION"
 ARTE_URL=""
+
+# Shut everything down 
+./StopToolkit.sh
 
 #Login to Artifactory using the fhe user
 echo "DOCKER LOGIN"
@@ -93,5 +94,8 @@ fi
 #Make A Notification in the Slack Channel about a new artifact in the repo
 pushd scripts/jenkins
 ./fhe_artifactory_notification_script.sh $SLACK_HOOK "Ubuntu" $BUILD_TYPE $ARTE_URL
+
+# Shut everything down 
+./StopToolkit.sh
 
 
