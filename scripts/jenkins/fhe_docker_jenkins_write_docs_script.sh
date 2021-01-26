865878406c72ccc4bdefd532c87a4ffb6e75d9ee
Gregory Boland <boland@us.ibm.com>
2021-01-26 16:49:21 -0500

    checking if files exist before writing

--- scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
+++ scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
@@ -30,6 +30,11 @@ set -x
 set -u
 set -e
 
+#Remove any previous lingering files from the last build
+if [ -d "fhe-toolkit-linux" ]; then
+    rm -rf fhe-toolkit-linux
+fi
+
 # Checkout specifically the gh-pages branch, so we can push our docs to it
 git clone -b gh-pages --single-branch git@github.com:IBM/fhe-toolkit-linux.git
 ls
