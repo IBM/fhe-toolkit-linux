d06a2de62c4cc8f833a2db6d238f952f89ed68a0
Gregory Boland <boland@us.ibm.com>
2021-01-27 09:10:31 -0500

    changing it to be checkout instead of clone

--- scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
+++ scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
@@ -45,7 +45,8 @@ echo $GIT_LOG
 if [[ "$GIT_LOG" == *"$H_FILES"* || "$GIT_LOG" == *"$CPP_FILES"* ]]; then 
     echo "CHANGES WERE MADE SO RE-GENERATE THE DOCS"
     # Checkout specifically the gh-pages branch, so we can push our docs to it
-    git clone -b gh-pages --single-branch git@github.com:IBM/fhe-toolkit-linux.git
+    #git clone -b gh-pages --single-branch git@github.com:IBM/fhe-toolkit-linux.git
+    git checkout gh-pages
 
     cd fhe-toolkit-linux
     #Call Write ML Helib Api Docs script so we generate docs and write them to the html folder
