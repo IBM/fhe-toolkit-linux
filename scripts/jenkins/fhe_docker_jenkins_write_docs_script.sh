1b139994056ef0eb29306198e419e3ed940b5088
Gregory Boland <boland@us.ibm.com>
2021-01-27 09:18:30 -0500

    forcing an update

--- scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
+++ scripts/jenkins/fhe_docker_jenkins_write_docs_script.sh
@@ -47,7 +47,8 @@ if [[ "$GIT_LOG" == *"$H_FILES"* || "$GIT_LOG" == *"$CPP_FILES"* ]]; then
     # Checkout specifically the gh-pages branch, so we can push our docs to it
     #git clone -b gh-pages --single-branch git@github.com:IBM/fhe-toolkit-linux.git
     git checkout gh-pages
-
+    git pull origin gh-pages
+    
     #cd fhe-toolkit-linux
     #Call Write ML Helib Api Docs script so we generate docs and write them to the html folder
     ./RenderMLhelibAPI.sh local/fhe-toolkit-ubuntu html
